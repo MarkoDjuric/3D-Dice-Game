ad9789f711544b43ae52152b23ecef7b5dd36025
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-12-02 23:54:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 
+
+    var style = document.querySelector("link[href='style.css']").sheet.CSSRules;
+
+    var cssAnimation = document.createElement('style');
+    //let rotatingBtn = document.getElementById("rotate")
+    //var face = document.getElementById('face')
+    //var roof = document.getElementById('roof')
+    let resetBtn = document.getElementById('reset');
+    let rollingBtn = document.getElementById("roll");
+    let rotateSides = ['X','Z'];
+    let numberArray = [ 270, 360, 450, 540, 630, 720, 810, 900];
+    let container = document.querySelector('.container');
+    let rotor = '';
+    var rand = 0;
+    var controledNumber = 0;
+    // tracking score and chanses for rolling
+    var humanScore = 0;
+    var computerScore = 0;
+    var humanGameOver = 0
