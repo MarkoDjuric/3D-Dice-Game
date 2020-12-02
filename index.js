c2f13a995f514307a6e0ba0d4dfa02654867d6e6
Marko Djuric <33701179+MarkoDjuric@users.noreply.github.com>
2020-12-02 23:55:27 +0100

    Update index.js

--- index.js
+++ index.js
@@ -17,4 +17,17 @@
     // tracking score and chanses for rolling
     var humanScore = 0;
     var computerScore = 0;
-    var humanGameOver = 0
+    var humanGameOver = 0;
+
+  function rotateY(){
+    rand = numberArray[Math.floor(Math.random() * 8)];
+
+
+    cssAnimation.type = 'text/css';
+    var rules = document.createTextNode('@keyframes rotateY {100% {transform: rotateY(' + rand + 'deg);}}');
+                                                                                                    
+    cssAnimation.appendChild(rules);
+    document.getElementsByTagName("head")[0].appendChild(cssAnimation);
+    container.style.animation = `rotateY 1s`;
+    
+  }
